95791043082a6a900920cf37cbf6de7684e02f99
HugoUrsa12 <115087733+HugoUrsa12@users.noreply.github.com>
2023-11-08 21:03:05 +0100

    Formulario terminado

--- Formulario de Hugo/Formulario de Hugo/MainWindow.xaml.cs
+++ Formulario de Hugo/Formulario de Hugo/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Microsoft.Win32;
 namespace Formulario_de_Hugo
 {
     /// <summary>
@@ -23,6 +23,92 @@ namespace Formulario_de_Hugo
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Txt_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+           
+            textBox.Text = string.Empty;
+            
+        }
+
+        private void Txt_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                if (textBox.Name == "txtDireccion")
+                {
+                    textBox.Text = "Direccion";
+                }
+                else if (textBox.Name == "txtCiudad")
+                {
+                    textBox.Text = "Ciudad";
+                }
+                else if (textBox.Name == "txtProvincia")
+                {
+                    textBox.Text = "Provincia";
+                }
+                else if (textBox.Name == "txtCodigoPostal")
+                {
+                    textBox.Text = "Código Postal";
+                }
+                else if (textBox.Name == "txtPais")
+                {
+                    textBox.Text = "País";
+                }
+               
+            }
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MenuItem menuItem = (MenuItem)sender;
+            string rolSeleccionado = menuItem.Header.ToString();
+            principalMenu.Header = rolSeleccionado;
+        }
+
+        private void GuardarButton(object sender, RoutedEventArgs e)
+        {
+            
+            string nombre = txtNombre.Text;
+            string apellidos = txtApellidos.Text;
+            string email = txtEmail.Text;
+            string telefono = txtTelefono.Text;
+            
+            var empleado = new { Nombre = nombre, Apellidos = apellidos, Email = email, Telefono = telefono };
+
+            dataGrid.Items.Add(empleado);
+
+  
+            txtNombre.Text = "";
+            txtApellidos.Text = "";
+            txtEmail.Text = "";
+            txtTelefono.Text = "";
+        }
+
+        private void CargarFoto_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.gif;*.bmp|Todos los archivos|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                BitmapImage bitmap = new BitmapImage(new Uri(filePath));
+                imagenCargada.Source = bitmap;
+            }
+        }
+
+        private void CancelarButton(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
         }
     }
 }
